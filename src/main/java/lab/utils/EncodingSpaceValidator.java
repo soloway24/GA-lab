b268697c61931d233ab78af13f4db1b4edbaacd8
Illia Kuznets <illiakuznets@gmail.com>
2024-03-18 03:11:52 +0200

    Added EncodingSpaceValidator and test for it.

---
+++ src/main/java/lab/utils/EncodingSpaceValidator.java
@@ -0,0 +1,44 @@
+package lab.utils;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class EncodingSpaceValidator {
+
+    private static final int PRECISION_BASE = 10;
+    private static final int BINARY_BASE = 2;
+
+    public static void validateEncodingSpace(int chromosomeLength, double minX, double maxX, int argumentPrecision) {
+        if (maxX < minX) {
+            throw new IllegalArgumentException("Provided min x " + minX + " is greater than max x " + maxX + " !");
+        }
+
+        BigDecimal encodingSpace = adjustScaleIfInteger(BigDecimal.valueOf(maxX - minX));
+        int encodingSpacePrecision = encodingSpace.scale();
+        if (encodingSpacePrecision > argumentPrecision) {
+            throw new IllegalArgumentException("Provided argument precision " + argumentPrecision
+                    + " is greater than actual encoding space precision " + encodingSpacePrecision + " !"
+                    + " Verify the precision of minX = " + minX + " and maxX = " + maxX + ".");
+        }
+
+        BigDecimal encodingsNumberMultiplier = BigDecimal.valueOf(PRECISION_BASE).pow(argumentPrecision);
+        BigInteger expectedEncodingsNumber = encodingSpace
+                .multiply(encodingsNumberMultiplier)
+                .add(BigDecimal.valueOf(1))
+                .toBigInteger();
+        BigInteger actualEncodingsNumber = BigInteger.valueOf(BINARY_BASE).pow(chromosomeLength);
+        if (!expectedEncodingsNumber.equals(actualEncodingsNumber)) {
+            throw new IllegalArgumentException("Chromosome with the provided length " + chromosomeLength
+                    + " encodes " + actualEncodingsNumber + " arguments with the precision of " + argumentPrecision
+                    + " while the expected number is " + expectedEncodingsNumber + " !");
+        }
+    }
+
+    private static BigDecimal adjustScaleIfInteger(BigDecimal value) {
+        try {
+            return new BigDecimal(value.toBigIntegerExact());
+        } catch (ArithmeticException e) {
+            return value;
+        }
+    }
+}
