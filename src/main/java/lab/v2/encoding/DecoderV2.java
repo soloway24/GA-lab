29240ea337930e0cae01bafc72600c35593fab6e
Illia Kuznets <illiakuznets@gmail.com>
2024-03-19 21:24:50 +0200

    Separated DecoderV2 from Decoder, made it singleton.

---
+++ src/main/java/lab/v2/encoding/DecoderV2.java
@@ -0,0 +1,68 @@
+package lab.v2.encoding;
+
+import lab.function.FitnessFunctionV2;
+import lab.model.Individual;
+import lab.parameters.Encoding;
+
+import java.util.Map;
+import java.util.function.Function;
+
+import static java.lang.Long.parseLong;
+import static java.util.Optional.ofNullable;
+import static lab.utils.Constants.BINARY_BASE;
+import static lab.utils.Constants.MAX_CHROMOSOME_LENGTH;
+
+public class DecoderV2 {
+
+    public static final DecoderV2 DECODER_V2 = new DecoderV2();
+
+    private static final Map<Encoding, Function<String, Long>> ENCODING_TO_DECODER =
+            Map.of(
+                    Encoding.STANDARD, DecoderV2::decodeStandardV2,
+                    Encoding.GRAY, DecoderV2::decodeGrayV2
+            );
+
+    private DecoderV2() {
+    }
+
+    public <ARG_T> ARG_T decodeV2(Individual individual, FitnessFunctionV2<ARG_T, ?> fitnessFunction) {
+        long decimalValue = getDecimalValueV2(individual);
+        return fitnessFunction.convertToX(decimalValue)
+                .orElseThrow(() -> new IllegalStateException("Provided function " + fitnessFunction.getName()
+                        + " does not support decoding of individuals."));
+    }
+
+    private long getDecimalValueV2(Individual individual) {
+        String binaryCode = individual.getBinaryCode();
+        verifyBinaryCode(binaryCode);
+
+        Encoding encoding = individual.getEncoding();
+        return ofNullable(ENCODING_TO_DECODER.get(encoding))
+                .map(decoder -> decoder.apply(binaryCode))
+                .orElseThrow(() -> new IllegalStateException("No decoder exists for the provided Encoding type - " + encoding));
+    }
+
+    private static long decodeStandardV2(String toDecode) {
+        return parseLong(toDecode, BINARY_BASE);
+    }
+
+    private static long decodeGrayV2(String toDecode) {
+        verifyBinaryCode(toDecode);
+        long decimalValue = parseLong(toDecode, BINARY_BASE);
+        long mask = decimalValue;
+        while ((mask = mask >> 1) != 0) {
+            decimalValue = decimalValue ^ mask;
+        }
+        return decimalValue;
+    }
+
+    private static void verifyBinaryCode(String toDecode) {
+        if (toDecode.length() > MAX_CHROMOSOME_LENGTH) {
+            throw new IllegalArgumentException("Decoding of individuals of the length greater than " + MAX_CHROMOSOME_LENGTH
+                    + "is not supported!");
+        }
+        if (!toDecode.matches("[01]+")) {
+            throw new IllegalArgumentException("String " + toDecode + " contains non-binary characters during binary decoding!");
+        }
+    }
+}
--- src/test/java/lab/encoding/DecoderV2Test.java
+++ src/test/java/lab/encoding/DecoderV2Test.java
@@ -10,16 +10,16 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
-import static lab.encoding.Decoder.decode;
 import static lab.model.Individual.ALL_100_ZEROS_INDIVIDUAL;
 import static lab.utils.Constants.MAX_CHROMOSOME_LENGTH;
+import static lab.v2.encoding.DecoderV2.DECODER_V2;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class DecoderTest {
+class DecoderV2Test {
 
     private static final Individual STANDARD_INDIVIDUAL = new Individual("1000010000", Encoding.STANDARD);
     private static final Individual GRAY_INDIVIDUAL = new Individual("1000010000", Encoding.GRAY);
@@ -37,7 +37,7 @@ class DecoderTest {
         double expectedX = DECIMAL_STANDARD / 100.0;
         when(doubleFunction.convertToX(DECIMAL_STANDARD)).thenReturn(of(expectedX));
 
-        double actualX = decode(STANDARD_INDIVIDUAL, doubleFunction);
+        double actualX = DECODER_V2.decodeV2(STANDARD_INDIVIDUAL, doubleFunction);
         assertEquals(expectedX, actualX);
     }
 
@@ -46,14 +46,14 @@ class DecoderTest {
         double expectedX = DECIMAL_GRAY / 100.0;
         when(doubleFunction.convertToX(DECIMAL_GRAY)).thenReturn(of(expectedX));
 
-        double actualX = decode(GRAY_INDIVIDUAL, doubleFunction);
+        double actualX = DECODER_V2.decodeV2(GRAY_INDIVIDUAL, doubleFunction);
         assertEquals(expectedX, actualX);
     }
 
     @Test
     public void whenBinaryCodeIsTooLongThenFailure() {
         assertThrows(IllegalArgumentException.class,
-                () -> decode(ALL_100_ZEROS_INDIVIDUAL, doubleFunction),
+                () -> DECODER_V2.decodeV2(ALL_100_ZEROS_INDIVIDUAL, doubleFunction),
                 "Decoding of individuals of the length greater than " + MAX_CHROMOSOME_LENGTH
                         + "is not supported!");
     }
@@ -61,7 +61,7 @@ class DecoderTest {
     @Test
     public void whenBinaryCodeContainsInvalidCharactersThenFailure() {
         assertThrows(IllegalArgumentException.class,
-                () -> decode(INVALID_INDIVIDUAL, doubleFunction),
+                () -> DECODER_V2.decodeV2(INVALID_INDIVIDUAL, doubleFunction),
                 "String " + INVALID_INDIVIDUAL.getBinaryCode()
                         + " contains non-binary characters during binary decoding!");
     }
@@ -72,7 +72,7 @@ class DecoderTest {
         when(nonDecodableDoubleFunction.getName()).thenReturn("Non-Decodable Double Function");
 
         assertThrows(IllegalStateException.class,
-                () -> decode(STANDARD_INDIVIDUAL, nonDecodableDoubleFunction),
+                () -> DECODER_V2.decodeV2(STANDARD_INDIVIDUAL, nonDecodableDoubleFunction),
                 "Provided function " + nonDecodableDoubleFunction.getName()
                         + " does not support decoding of individuals.");
     }
